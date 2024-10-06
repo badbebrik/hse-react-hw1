ca0874719fd6b659b558e5ea99a9d9b14e65223a
Viktoria Serikova <viktoria.serikova2016@yandex.ru>
2024-10-06 22:44:59 +0300

    Implement easy-medium tasks

--- easy.ts
+++ easy.ts
@@ -1,7 +1,9 @@
-export type MyPick<T, K> = unknown;
+export type MyPick<T, K extends keyof T> = {
+    [P in K]: T[P]; // Проходимся по каждому ключу K и добавляем его в новый тип
+  };
 
-export type NOfArray<ArrayObj, N> = unknown;
+export type NOfArray<ArrayObj extends any[], N extends number> = ArrayObj[N]; // Извлекаем тип N-го элемента массива
 
-export type Unshift<ArrayType, Element> = unknown;
+export type Unshift<ArrayType extends unknown[], Element> = [Element, ...ArrayType]; // Создаем новый массив с Element первым, затем копируем остальные элементы из ArrayType
 
-export type MyExclude<T, U> = unknown;
+export type MyExclude<T, U> = T extends U ? never : T; // Если T подходит под тип U, исключаем его, иначе оставляем T
--- medium.ts
+++ medium.ts
@@ -1,7 +1,29 @@
-export type DeepPartial<T> = unknown;
+export type DeepPartial<T> = T extends Function
+  ? T // Функции не трогаем
+  : T extends object // Обрабатываем только объекты и массивы
+  ? T extends Array<infer U> // Если  массив, рекурсивно применяем DeepPartial к элементам
+    ? Array<DeepPartial<U>>
+    : { [P in keyof T]?: DeepPartial<T[P]> } // Делаем ключи объектов опциональными
+  : T; // Примитивные типы не трогаем
 
-export type MyCapitalize<T> = unknown;
+export type MyCapitalize<T extends string> =
+  T extends `${infer FirstLetter}${infer RestContent}` // Бьем строку на первую букву и остальную часть
+    ? `${Uppercase<FirstLetter>}${RestContent}` // Переводим первую букву в верхний регистр, остальное оставляем
+    : T; // Если строка пустая или не подходит, возвращаем её без изменений
 
-export type DeepMutable<T> = unknown;
+export type DeepMutable<T> = T extends object
+  ? T extends Function // Функции не трогаем
+    ? T
+    : T extends Array<infer U> // Если массив, рекурсивно идем по элементам и применяем DeepMutable
+    ? Array<DeepMutable<U>>
+    : {
+        -readonly [P in keyof T]: DeepMutable<T[P]>; // Убираем readonly, применяем рекурсивно DeepMutable к ключам
+      }
+  : T; // Примитивные типы не трогаем
 
-export type ParseURLParams<StringElem> = unknown;
+export type ParseURLParams<StringElem extends string> =
+  StringElem extends `${infer BeforeParam}:${infer ParamName}/${infer RestOfURL}` // Ищем параметр в формате ':param', продолжаем разбор после '/'
+    ? ParamName | ParseURLParams<RestOfURL> // Находим параметр и продолжаем искать дальше в оставшейся строке
+    : StringElem extends `${infer BeforeParam}:${infer ParamName}`
+    ? ParamName // Если это последний параметр (нет больше / в строке), возвращаем его
+    : never; // Если нет параметров - never
