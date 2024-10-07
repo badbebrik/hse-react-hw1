f3955706556ca6c9d61f02cbbb9a00baffba6a65
Viktoria Serikova <viktoria.serikova2016@yandex.ru>
2024-10-07 04:01:41 +0300

    Implement hard tasks

--- hard.ts
+++ hard.ts
@@ -1,3 +1,27 @@
-export type Camelize<ObjectType> = unknown;
+type SnakeToCamelCase<S extends string> = S extends `${infer T}_${infer U}`
+  ? `${T}${Capitalize<SnakeToCamelCase<U>>}` // Соединяем первую часть и преобразованную вторую
+  : S; // Если подчеркиваний не осталось, возвращаем строку
 
-export type DeepPick<T, Paths> = unknown;
+export type Camelize<ObjType> = ObjType extends Function
+  ? ObjType // Функции не трогаем
+  : ObjType extends Array<infer U> // Если массив, применяем Camelize к элементам массива
+  ? Array<Camelize<U>>
+  : ObjType extends object // Если объект - преобразовываем ключи
+  ? {
+      [K in keyof ObjType as SnakeToCamelCase<Extract<K, string>>]: Camelize<
+        ObjType[K]
+      >;
+    }
+  : // Преобразуем ключи объекта в camelCase и рекурсивно применяем Camelize ко всем значениям
+    ObjType; // Примитивы не трогаем
+
+export type DeepPick<
+  T,
+  Paths extends string
+> = Paths extends `${infer Key}.${infer Rest}`
+  ? Key extends keyof T
+    ? { [K in Key]: DeepPick<T[K], Rest> } // Рекурсивно выбираем вложенные ключи
+    : never
+  : Paths extends keyof T
+  ? { [K in Paths]: T[K] } // Если путь не содержит вложенности (нет '.'), просто выбираем ключ
+  : never;
